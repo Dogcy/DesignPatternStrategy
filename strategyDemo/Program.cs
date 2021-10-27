1399d6d48b1015efbc5855f27d6e5f872807b93e
DOGCY <ccy810127@gmail.com>
2021-10-28 02:05:35 +0800

    firstcommit

---
+++ strategyDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace strategyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context;
+
+            context = new Context(new ConcreateStrategyA());
+            context.ContextInterFace();
+
+            context = new Context(new ConcreateStrategyB());
+            context.ContextInterFace();
+
+            context = new Context(new ConcreateStrategyC());
+            context.ContextInterFace();
+        }
+    }
+}
---
+++ strategyDemo/Strategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strategyDemo
+{
+    abstract class Strategy
+    {
+        public abstract void Algorithminterface();
+    }
+    class ConcreateStrategyA : Strategy
+    {
+        public override void Algorithminterface()
+        {
+            Console.WriteLine("演算法A的實現");
+        }
+    }
+    class ConcreateStrategyB : Strategy
+    {
+        public override void Algorithminterface()
+        {
+            Console.WriteLine("演算法B的實現");
+        }
+    }
+    class ConcreateStrategyC : Strategy
+    {
+        public override void Algorithminterface()
+        {
+            Console.WriteLine("演算法C的實現");
+        }
+    }
+
+
+    class Context
+    {
+        private Strategy strategy;
+        public Context(Strategy strategy)
+        {
+            this.strategy = strategy;
+        }
+        public void ContextInterFace()
+        {
+            this.strategy.Algorithminterface();
+        }
+    }
+}
